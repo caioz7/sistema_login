ba9951e867aa75fefa018721710cf1fe69f5e02b
Caio César Dantas <13774762+caioz7@users.noreply.github.com>
2018-10-31 20:13:44 -0300

    Create criar_usuarios.sql

---
+++ criar_usuarios.sql
@@ -0,0 +1,4 @@
+INSERT INTO users(nome, email, password) VALUES
+('Usuário 1', 'user1@user.com', '2d29b962490320f821db80cddf6ed4b6e4ac7498'),
+('Usuário 2', 'user2@user.com', '2d29b962490320f821db80cddf6ed4b6e4ac7498'),
+('Usuário 3', 'user3@user.com', '2d29b962490320f821db80cddf6ed4b6e4ac7498');
