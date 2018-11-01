5b39abc7ea5868fb420c0902cd8d6355597d5a6e
Max Rogério <max10rogerio@hotmail.com>
2018-11-01 09:40:12 -0300

    simplificado a validação dos campos

--- agenda.php
+++ agenda.php
@@ -11,13 +11,14 @@ error_reporting(0);
 
 // Verificar se foi enviando dados via POST
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $id = (isset($_POST["id"]) && $_POST["id"] != null) ? $_POST["id"] : "";
-    $nome = (isset($_POST["nome"]) && $_POST["nome"] != null) ? $_POST["nome"] : "";
-    $email = (isset($_POST["email"]) && $_POST["email"] != null) ? $_POST["email"] : "";
-    $celular = (isset($_POST["celular"]) && $_POST["celular"] != null) ? $_POST["celular"] : "";
+    // ?? == isset e empty
+    $id = $_POST["id"] ?? "";
+    $nome = $_POST["nome"] ?? "";
+    $email = $_POST["email"] ?? "";
+    $celular = $_POST["celular"] ?? "";
 } else if (!isset($id)) {
     // Se não se não foi setado nenhum valor para variável $id
-    $id = (isset($_GET["id"]) && $_GET["id"] != null) ? $_GET["id"] : "";
+    $id = $_GET["id"] ?? "";
 }
 
 // busca a instancia do banco de dados
@@ -92,34 +93,16 @@ if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "del" && $id != "") {
   <form action="?act=save" method="POST" name="form1" >
       <h1>Agenda de contatos</h1>
       <hr>
-      <input type="hidden" name="id" <?php
-      
-      // Preenche o id no campo id com um valor "value"
-      if (isset($id) && $id != null || $id != "") {
-          echo "value=\"{$id}\"";
-      }
-      ?> />
+      <input type="hidden" name="id" value='<?php echo $id ?? ''; ?>' />
       Nome:
-     <input type="text" name="nome" <?php
-
-     // Preenche o nome no campo nome com um valor "value"
-     if (isset($nome) && $nome != null || $nome != "") {
-         echo "value=\"{$nome}\"";
-     }
-     ?> />
-     E-mail:
-     <input type="text" name="email" <?php
-
-     // Preenche o email no campo email com um valor "value"
-     if (isset($email) && $email != null || $email != "") {
-         echo "value=\"{$email}\"";
-     }
-     ?> />
-     Celular:
-     <input type="text" name="celular" <?php echo isset($celular) && !empty($celular) ? "value='{$celular}'" : '';?> />
-     <input type="submit" value="salvar" />
-     <input type="reset" value="Novo" />
-     <hr>
+      <input type="text" name="nome" value='<?php echo $nome ?? ''; ?>' />
+      E-mail:
+      <input type="text" name="email" value='<?php echo $email ?? ''; ?>' />/>
+      Celular:
+      <input type="text" name="celular" value='<?php echo $celular ?? ''; ?>' />
+      <input type="submit" value="salvar" />
+      <input type="reset" value="Novo" />
+      <hr>
   </form>
         <table border="1" width="100%">
             <tr>
