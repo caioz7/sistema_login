d4d131e766b4977277f5b905b2d430c5cf28a587
Aluno <Aluno>
2018-10-31 21:50:03 -0300

    Feito menu e home

--- home.php
+++ home.php
@@ -4,22 +4,9 @@
     require_once 'conexao.php';
     require_once 'verifica_sessao.php';
     require_once './assets/header.php';
+    require_once './assets/menu.php';
 ?>
-    <h1>
+    <h1 class="text-center">
         HOME do Sistema
     </h1>
-
-    <p>
-        Seja bem vindo, <?php echo $_SESSION['user_name']; ?>.
-    </p>
-    
-    <a href="javascript:window.history.go(-1)">
-        Voltar
-    </a>
-
-    <br/><br/><br/>
-    
-    <a href="logout.php">
-        Sair
-    </a>
 <?php require_once './assets/footer.php'; ?>
